5f1e1c8235c14b7d9c3c71469463ed32f2f4f367
sg98809880 <150770403+sg98809880@users.noreply.github.com>
2026-06-23 19:18:25 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [{ protocol: "https", hostname: "**" }],
-  },
+reactStrictMode: true,
+images: {
+remotePatterns: [
+{
+protocol: "https",
+hostname: "**",
+},
+],
+},
 };
 
 export default nextConfig;
